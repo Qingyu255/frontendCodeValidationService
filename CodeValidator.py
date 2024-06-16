fc04bee1495c4d91db855be75277cd8d5500e314
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-06-16 22:53:45 +0800

    selenium validation

--- CodeValidator.py
+++ CodeValidator.py
@@ -1,17 +1,35 @@
-class CodeValidator:
-    def __init__(self):
-        srcDoc = ''
+import time
+import subprocess
+from multiprocessing import Process
 
+class CodeValidator:
     def validate_HTML_CSS_JS_Code(self, srcDoc):
         """checks if code has invalid syntax"""
-        error_message = ""
+        with open("index.html", "w") as infile:
+            infile.write(srcDoc)
 
-        # code validation here...
+        p = Process(target=self.start_server)
+        p.start()
+        time.sleep(3)
 
-        error_message = "Password Input absent"
-        return {
-            "isCorrectSolution" : True,
-            "error_message" : error_message
-        }
-    
-    
+        result = subprocess.run(
+            ["env/bin/python", "test_script.py"], 
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        print(result)
+
+        p.terminate()
+        p.join()
+
+        if result.returncode == 1:
+            return {
+                "isCorrectSolution" : False,
+                "error_message" : result.stderr
+            }
+        return { "isCorrectSolution": True } 
+
+    def start_server(self):
+        print("starting server")
+        subprocess.run(["python3", "-m", "http.server"])
--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from CodeValidator import CodeValidator
+import uvicorn
 # from typing import List, Dict, Optional
 
 app = FastAPI()
@@ -34,15 +35,19 @@ async def helloWorld():
 
 @app.post("/process_html/")
 async def process_html(data: srcDocString):
-    srcDocString = data.content
+    print(data)
+    code = data.content
     # do stuff
     # Here you can process the HTML content, e.g., store it in the database, test, etc.
 
     # TODO: validation service returns result (decide return type)
 
-    validationResult = code_validator.validate_HTML_CSS_JS_Code(srcDocString)
+    validationResult = code_validator.validate_HTML_CSS_JS_Code(code)
     payload = {
         "message": "HTML String content received and processed.. wtv not", 
         "validation_outcome": validationResult
     }
     return payload
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=5000)
